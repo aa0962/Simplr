b5f975141773f7c2babcbe596827ccc240010dd8
aa0962 <aa0962@srmist.edu.in>
2024-06-29 19:31:18 +0530

    init: fronted client

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <>
+    <div className='text-2xl'>
+      Project Initialised
+    </div>
+    </>
+  )
+}
+
+export default App
