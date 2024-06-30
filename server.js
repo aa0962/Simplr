4569455e0e03390e512d77a285443c3e7a2c1f68
aa0962 <aa0962@srmist.edu.in>
2024-06-30 14:21:35 +0530

    fix: prod

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-// Connect to database
-connectDB();
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -16,8 +12,12 @@ app.use(express.json());
 // Routes
 app.use('/api/movies', movieRoutes);
 
+// Connect to database
+connectDB();
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+  
   console.log(`Server running on port ${PORT}`);
 });
